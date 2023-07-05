7396bc82b6ad2e8eb305641fcebd69c27c60d4fc
jasm1tha <110765928+jasm1tha@users.noreply.github.com>
2023-07-05 13:33:28 -0400

    Update todolist_app.py

--- todolist_app.py
+++ todolist_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+import streamlit as st
+
+st.title('Jasmitha''s Streamlit App')
+st.write('Welcome to my app!')
+
+name = st.text_input('Enter your name')
+st.write('Hello,', name)
 
